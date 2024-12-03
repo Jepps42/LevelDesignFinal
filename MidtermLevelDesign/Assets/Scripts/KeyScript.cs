0a3d1901a0693fc12e999cddb751deec08edd761
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-12-03 13:04:58 -0500

    Key script added

---
+++ MidtermLevelDesign/Assets/Scripts/KeyScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+
+    //Variables 
+    public GameObject doorCollider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorCollider.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            doorCollider.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
