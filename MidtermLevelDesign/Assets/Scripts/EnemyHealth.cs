988da8c5a3b0fcac17d9f4c5dc434da0eaa8add8
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-12-11 11:35:54 -0500

    Added enemy health script, needs checking

--- MidtermLevelDesign/Assets/Scripts/EnemyHealth.cs
+++ MidtermLevelDesign/Assets/Scripts/EnemyHealth.cs
@@ -1,33 +1,54 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class EnemyHealth : MonoBehaviour
+public class EnemyHealthBar : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int curHealth = 100;
-    public float healthBarLength;
-    // Use this for initialization
+    [Header("Health Settings")]
+    public float maxHealth = 100f; // Maximum health
+    private float currentHealth;
+
+    [Header("UI Components")]
+    public Slider healthBarSlider; // The UI slider for health
+    public Vector3 offset = new Vector3(0, 2, 0); // Offset above the enemy
+
+    private Camera mainCamera; // Reference to the main camera
+
     void Start()
     {
-        healthBarLength = Screen.width / 2;
+        currentHealth = maxHealth; // Initialize health
+        healthBarSlider.maxValue = maxHealth; // Set slider max value
+        healthBarSlider.value = currentHealth; // Set initial slider value
+        mainCamera = Camera.main; // Cache the main camera
     }
 
-    // Update is called once per frame
-
-
-    void OnGUI()
+    void Update()
     {
-        GUI.Box(new Rect(10, 40, healthBarLength, 20), curHealth + "/" + maxHealth);
-    }
-
-    public void AdjustCurrentHealth(int adjust)
-    {
-        curHealth -= adjust;
-        healthBarLength = (Screen.width / 4) * (curHealth / (float)maxHealth);
-        if (curHealth <= 0)
+        // Update the position of the health bar to follow the enemy
+        if (healthBarSlider != null)
         {
-            Destroy(this.gameObject);
+            Vector3 worldPosition = transform.position + offset;
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(worldPosition);
+            healthBarSlider.transform.position = screenPosition;
         }
-      
     }
+
+    /*public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        healthBarSlider.value = currentHealth;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Add death behavior here (e.g., destroy enemy, play animation)
+        Destroy(gameObject);
+    }*/
 }
